ab68db67f8e9d04947b801e6467c1eeb7f34b17d
Jamal2911 <80721444+Jamal2911@users.noreply.github.com>
2025-07-04 02:40:45 +0200

    Wrap editor scripts with UNITY_EDITOR preprocessor
    Added #if UNITY_EDITOR guards to GlobalKeyEventHandler.cs and GizmosDrawer.cs to ensure these scripts are only included in editor builds. This prevents potential build errors when targeting non-editor platforms.

--- Assets/src/EditorTools/GameObjectPicker/GlobalKeyEventHandler.cs
+++ Assets/src/EditorTools/GameObjectPicker/GlobalKeyEventHandler.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using UnityEditor;
 using UnityEngine;
@@ -54,3 +55,4 @@ namespace JamalArouna.EditorTools
         }
     }   
 }
+#endif
--- Assets/src/Utilities/GizmosDrawer.cs
+++ Assets/src/Utilities/GizmosDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -31,3 +32,4 @@ namespace JamalArouna.Utilities
         }
     }
 }
+#endif
