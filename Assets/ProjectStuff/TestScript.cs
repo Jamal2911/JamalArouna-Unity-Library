31dbefcc21b3c0f211422ab42298a259739da6a6
Jamal2911 <80721444+Jamal2911@users.noreply.github.com>
2025-07-04 03:14:36 +0200

    Add TestScript and extend IState interface
    Added a new TestScript MonoBehaviour in ProjectStuff. Extended the IState interface file by introducing IParameterizedState for states requiring dynamic parameters.

---
+++ Assets/ProjectStuff/TestScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class TestScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
--- Assets/src/Utilities/Interfaces/IState.cs
+++ Assets/src/Utilities/Interfaces/IState.cs
@@ -1,7 +1,7 @@
 namespace JamalArouna.Utilities.Interfaces
 {
     /// <summary>
-    /// Interface for defining a state in a state machine.
+    /// Basic state interface for a state machine.
     /// </summary>
     /// <remarks>
     /// Created by Jamal Arouna, 2025.
@@ -22,5 +22,32 @@ namespace JamalArouna.Utilities.Interfaces
         /// Called when exiting the state.
         /// </summary>
         void Exit();
+    }
+
+    /// <summary>
+    /// Extended interface for states that require dynamic parameters on calls.
+    /// </summary>
+    /// <remarks>
+    /// Created by Jamal Arouna, 2025.
+    /// </remarks>
+    public interface IParameterizedState
+    {
+        /// <summary>
+        /// Called when entering the state with an optional parameter.
+        /// </summary>
+        /// <param name="parameter">Optional input data</param>
+        void Enter(object parameter);
+
+        /// <summary>
+        /// Called every frame while the state is active, with an optional parameter.
+        /// </summary>
+        /// <param name="parameter">Optional input data</param>
+        void Update(object parameter);
+
+        /// <summary>
+        /// Called when exiting the state with an optional parameter.
+        /// </summary>
+        /// <param name="parameter">Optional input data</param>
+        void Exit(object parameter);
     }
 }
