b3742dbd85d8fccc152af62d6f1770c09d85c133
Jamal2911 <80721444+Jamal2911@users.noreply.github.com>
2025-07-07 01:33:59 +0200

    Update OneTimeEventTrigger.cs

--- Assets/JamalArouna.Library/Utilities/OneTimeEventTrigger.cs
+++ Assets/JamalArouna.Library/Utilities/OneTimeEventTrigger.cs
@@ -12,11 +12,22 @@ namespace JamalArouna.Utilities
     public class OneTimeEventTrigger
     {
         private bool triggered = false;
+
+        /// <summary>
+        /// The action to execute once when triggered.
+        /// </summary>
         private readonly Action action;
 
         /// <summary>
-        /// Executes the provided action only once when this method is called.
-        /// Subsequent calls have no effect until <see cref="Reset"/> is called.
+        /// Initializes a new instance of the <see cref="OneTimeEventTrigger"/> class with the specified action.
+        /// </summary>
+        /// <param name="action">The action to execute only once. Cannot be null.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is null.</exception>
+        public OneTimeEventTrigger(Action action) => this.action = action ?? throw new ArgumentNullException(nameof(action));
+
+        /// <summary>
+        /// Executes the stored action if it has not been executed yet.
+        /// Marks the trigger as used. Further calls do nothing until <see cref="Reset"/> is called.
         /// </summary>
         public void TryTrigger()
         {
@@ -28,7 +39,7 @@ namespace JamalArouna.Utilities
         }
 
         /// <summary>
-        /// Resets the trigger, allowing the action to be executed again on the next call to <see cref="TryTrigger"/>.
+        /// Resets the trigger, allowing the stored action to be executed again on the next call to <see cref="TryTrigger"/>.
         /// </summary>
         public void Reset() => triggered = false;
     }
