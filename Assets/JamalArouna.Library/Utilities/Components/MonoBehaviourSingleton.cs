b9b637ba739053aed2b277d37845039414992932
Jamal Arouna <80721444+Jamal2911@users.noreply.github.com>
2026-05-17 20:49:32 +0200

    Move PersistentSingleton into Singleton file
    Remove the separate MonoBehaviourPersistentSingleton.cs file and inline MonoBehaviourPersistentSingleton<T> into MonoBehaviourSingleton.cs (remarks year updated to 2026). Add .meta files for LockSystem.cs and HandleRenderersOnStart.cs. This consolidates singleton implementations into one file to simplify maintenance and reduce file clutter.

--- Assets/JamalArouna.Library/Utilities/Components/MonoBehaviourSingleton.cs
+++ Assets/JamalArouna.Library/Utilities/Components/MonoBehaviourSingleton.cs
@@ -49,4 +49,26 @@ namespace JamalArouna.Library.Utilities.Components
                 Destroy(gameObject);
         }
     }
+    
+    /// <summary>
+    /// Abstract base class for persistent MonoBehaviour-based singletons.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The MonoBehaviour type that should have exactly one persistent instance.
+    /// </typeparam>
+    /// <remarks>
+    /// Created by Jamal Arouna, 2026.
+    /// </remarks>
+    public abstract class MonoBehaviourPersistentSingleton<T> : MonoBehaviourSingleton<T> where T : MonoBehaviour
+    {
+        /// <summary>
+        /// Initializes the singleton instance and marks its GameObject as persistent.
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
+
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 }
